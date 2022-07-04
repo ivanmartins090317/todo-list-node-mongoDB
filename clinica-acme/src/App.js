08c7bf3123bccaf67741c99e7257749685f5809e
Ivan Martins <ivanmartins.ilha@gmail.com>
2022-07-04 11:21:21 -0300

    cria componets de cadastro e rendetiza pacients

--- clinica-acme/src/App.js
+++ clinica-acme/src/App.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
 
+import Cadastro from "./components/cadastro";
+
 function App() {
   const [patient, setPatient] = useState([
     {
       name: "ivan",
-      data_nasciment: "07-01-1986",
+      data_nascimento: "07-01-1986",
       cpf: 35556654,
       sexo: "masculino",
       endereço: "rua...",
       status: true,
     },
   ]);
-  return;
-  <>
-    <div></div>;
-  </>;
+  return (
+    <>
+      <div>
+        <Cadastro patient={patient} />
+      </div>
+    </>
+  );
 }
 
 export default App;
